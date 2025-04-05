dbcfcdfa47213d11c3dcf04b8504262312bb8c33
Alex Martishin <asmartishin@gmail.com>
2025-04-05 21:18:37 +0200

    Add decoding logic

--- geohash.go
+++ geohash.go
@@ -1,50 +1,54 @@
 package geohash
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	base32Codes = "0123456789bcdefghjkmnpqrstuvwxyz"
 	// Each base32 character represents exactly 5 bits of a sequence.
 	bitsPerChar = 5
 	// Base32 mask 0x1F, equivalent to 00011111 in binary.
-	bitsMask  = (1 << bitsPerChar) - 1
-	coordBits = 30
-	totalBits = 60
+	bitsMask     = (1 << bitsPerChar) - 1
+	oneCoordBits = 30
+	totalBits    = 60
 )
 
-func GenerateGeohash(x, y float64) string {
-	var xBits, yBits uint32
+// GenerateGeohash encodes the (longitude, latitude) into a 12-character geohash.
+func GenerateGeohash(lng, lat float64) string {
+	var lngBits, latBits uint32
 
-	xs := []float64{-180.0, 0.0, 180.0}
-	ys := []float64{-90.0, 0.0, 90.0}
+	lngLimits := []float64{-180.0, 0.0, 180.0}
+	latLimits := []float64{-90.0, 0.0, 90.0}
 
-	for i := coordBits - 1; i >= 0; i-- {
-		processCoordinate(x >= xs[1], &xBits, xs, i)
-		processCoordinate(y >= ys[1], &yBits, ys, i)
+	for i := oneCoordBits - 1; i >= 0; i-- {
+		processCoordinate(lng >= lngLimits[1], &lngBits, lngLimits, i)
+		processCoordinate(lat >= latLimits[1], &latBits, latLimits, i)
 	}
 
-	resultBits := interleaveBits(xBits, yBits)
+	resultBits := interleaveBits(lngBits, latBits)
 
 	return encodeGeohash(resultBits)
 }
 
-func processCoordinate(rightOfMedian bool, bits *uint32, coords []float64, bitPosition int) {
+func processCoordinate(rightOfMedian bool, coordBits *uint32, coordLimits []float64, bitPosition int) {
 	if rightOfMedian {
-		*bits |= 1 << bitPosition
-		coords[0] = coords[1]
+		*coordBits |= 1 << bitPosition
+		coordLimits[0] = coordLimits[1]
 	} else {
-		coords[2] = coords[1]
+		coordLimits[2] = coordLimits[1]
 	}
 
-	coords[1] = (coords[0] + coords[2]) / 2.0
+	coordLimits[1] = (coordLimits[0] + coordLimits[2]) / 2.0
 }
 
-func interleaveBits(xBits, yBits uint32) uint64 {
+func interleaveBits(lngBits, latBits uint32) uint64 {
 	var resultBits uint64
 
-	for i := coordBits - 1; i >= 0; i-- {
-		resultBits |= uint64((xBits>>i)&1) << (2*i + 1)
-		resultBits |= uint64((yBits>>i)&1) << (2 * i)
+	for i := oneCoordBits - 1; i >= 0; i-- {
+		resultBits |= uint64((lngBits>>i)&1) << (2*i + 1)
+		resultBits |= uint64((latBits>>i)&1) << (2 * i)
 	}
 
 	return resultBits
@@ -55,9 +59,54 @@ func encodeGeohash(resultBits uint64) string {
 	geohash.Grow(totalBits / bitsPerChar)
 
 	for i := bitsPerChar; i <= totalBits; i += bitsPerChar {
-		pos := (resultBits >> (totalBits - i)) & bitsMask
-		geohash.WriteByte(base32Codes[pos])
+		idx := (resultBits >> (totalBits - i)) & bitsMask
+		geohash.WriteByte(base32Codes[idx])
 	}
 
 	return geohash.String()
+}
+
+// DecodeGeohash decodes a 12-character geohash string back to the (longitude, latitude)
+func DecodeGeohash(hash string) (lng, lat float64, err error) {
+	if len(hash) != totalBits/bitsPerChar {
+		return 0, 0, fmt.Errorf("invalid geohash length: got %d, expected %d", len(hash), totalBits/bitsPerChar)
+	}
+	var inputBits uint64
+	for i := 0; i < len(hash); i++ {
+		idx := strings.IndexByte(base32Codes, hash[i])
+		if idx < 0 {
+			return 0, 0, fmt.Errorf("invalid character in geohash: %c", hash[i])
+		}
+		inputBits = (inputBits << bitsPerChar) | uint64(idx)
+	}
+
+	lngBits, latBits := deInterleaveBits(inputBits)
+
+	lng = decodeCoordinate(-180.0, 180.0, lngBits)
+	lat = decodeCoordinate(-90.0, 90.0, latBits)
+
+	return lng, lat, nil
+}
+
+func deInterleaveBits(resultBits uint64) (uint32, uint32) {
+	var lngBits, latBits uint32
+	for i := oneCoordBits - 1; i >= 0; i-- {
+		lngBits |= uint32((resultBits>>(2*i+1))&1) << i
+		latBits |= uint32((resultBits>>(2*i))&1) << i
+	}
+
+	return lngBits, latBits
+}
+
+func decodeCoordinate(coordMin float64, coordMax float64, coordBits uint32) float64 {
+	// Decode coordinate using binary subdivision.
+	for i := oneCoordBits - 1; i >= 0; i-- {
+		coordMid := (coordMin + coordMax) / 2
+		if ((coordBits >> i) & 1) == 1 {
+			coordMin = coordMid
+		} else {
+			coordMax = coordMid
+		}
+	}
+	return (coordMin + coordMax) / 2
 }
--- geohash_test.go
+++ geohash_test.go
@@ -1,37 +1,51 @@
 package geohash_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/martishin/geohash"
 )
 
 type TestCase struct {
-	expectedInt uint64
-	expectedStr string
-	lat         float64
-	lng         float64
+	hash string
+	lat  float64
+	lng  float64
+}
+
+var testCases = []TestCase{
+	{"sb54v4xk18jg", 0.497818518, 38.198505253},
+	{"00upjeyjb54g", -84.529178182, -174.125057287},
+	{"kkfwu0udnhxm", -17.090238388, 14.947853282},
+	{"gp2cx4ywjhyj", 86.06108453, -43.628546008},
+	{"h0g4tmrp0cut", -85.311745894, 4.459114168},
+	{"v471duxnbttv", 57.945830289, 49.349241965},
+	{"h78n33z47k3j", -69.203844118, 11.314685805},
+	{"gvtw7yer4bhh", 77.073040753, -3.346243298},
+	{"0fqwy0pgxxwj", -76.156584583, -136.834730089},
+	{"dj53wuppzfrx", 28.411988257, -85.123100792},
+	{"rmbn08wvubcz", -11.597823607, 146.281448853},
 }
 
 func TestGenerateGeohash(t *testing.T) {
-	var testcases = []TestCase{
-		{0xc28a4d93b20a22f8, "sb54v4xk18jg", 0.497818518, 38.198505253},
-		{0x003558b7d15148f1, "00upjeyjb54g", -84.529178182, -174.125057287},
-		{0x949dcd034ca43b30, "kkfwu0udnhxm", -17.090238388, 14.947853282},
-		{0x7d44be93dc8c3d1f, "gp2cx4ywjhyj", 86.06108453, -43.628546008},
-		{0x801e4ccef502f590, "h0g4tmrp0cut", -85.311745894, 4.459114168},
-		{0xd90e166bb45673b6, "v471duxnbttv", 57.945830289, 49.349241965},
-		{0x81d1418fe43c871f, "h78n33z47k3j", -69.203844118, 11.314685805},
-		{0x7ef3c3f9b722a109, "gvtw7yer4bhh", 77.073040753, -3.346243298},
-		{0x03adcf02afef7916, "0fqwy0pgxxwj", -76.156584583, -136.834730089},
-		{0x644a3e6ab5fbafd6, "dj53wuppzfrx", 28.411988257, -85.123100792},
-		{0xbcd540239bd297fb, "rmbn08wvubcz", -11.597823607, 146.281448853},
-	}
-
-	for _, tc := range testcases {
+	for _, tc := range testCases {
 		result := geohash.GenerateGeohash(tc.lng, tc.lat)
-		if result != tc.expectedStr {
-			t.Errorf("GenerateGeohash(%f, %f) = %s; expected %s", tc.lng, tc.lat, result, tc.expectedStr)
+		if result != tc.hash {
+			t.Errorf("GenerateGeohash(%f, %f) = %s; expected %s", tc.lng, tc.lat, result, tc.hash)
+		}
+	}
+}
+
+func TestDecodeGeohash(t *testing.T) {
+	const tolerance = 1e-6
+
+	for _, tc := range testCases {
+		lng, lat, err := geohash.DecodeGeohash(tc.hash)
+		if err != nil {
+			t.Errorf("DecodeGeohash(%s) returned error: %v", tc.hash, err)
+		}
+		if math.Abs(lng-tc.lng) > tolerance || math.Abs(lat-tc.lat) > tolerance {
+			t.Errorf("DecodeGeohash(%s) = (%f, %f); expected (%f, %f)", tc.hash, lng, lat, tc.lng, tc.lat)
 		}
 	}
 }
